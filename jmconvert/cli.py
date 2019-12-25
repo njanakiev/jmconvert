c18ece7ae3f09dc7a0887699446adaa25f148e64
Nikolai Janakiev <nikolai.janakiev@gmail.com>
2019-12-26 00:03:04 +0100

    Refactor and add processing for multiple notebooks

--- jmconvert/cli.py
+++ jmconvert/cli.py
@@ -1,12 +1,9 @@
 from __future__ import print_function
 import os
-import re
-import sys
-import shutil
-import datetime
 import argparse
-import nbconvert
-import nbformat
+
+import parse
+#from . import parse
 
 
 def main():
@@ -17,139 +14,41 @@ def main():
         description='Jupyter notebook to Jekyll markdown converter')
     parser.add_argument(action='store', dest='ipynb_filepath',
                         help='Jupyter notebook path')
-    parser.add_argument('-f', action='store', dest='jekyll_folder',
-                        help='Jekyll folder path', required=False)
-    parser.add_argument('-a', action='store', dest='assets_folder',
-                        help='Assets folder path', required=False)
-    parser.add_argument('-p', action='store', dest='posts_folder',
-                        help='Posts folder path', required=False)
-    parser.add_argument('-t', action='store_true', default=False,
-                        help='Use current time as date')
+    parser.add_argument(action='store', dest='jekyll_folder',
+                        help='Jekyll folder path')
+    parser.add_argument('-o', action='store_true', default=False,
+                        help='Overwrite existing', dest='overwrite')
 
+    # Get filepaths and settings
     args = parser.parse_args()
     ipynb_filepath = args.ipynb_filepath
-    posts_folder   = args.posts_folder  if args.posts_folder  else '_posts'
-    assets_folder  = args.assets_folder if args.assets_folder else 'assets'
     jekyll_folder  = args.jekyll_folder if args.jekyll_folder else ''
-    current_time   = args.t
-    assets_path    = os.path.join(jekyll_folder, assets_folder)
-    posts_path     = os.path.join(jekyll_folder, posts_folder)
+    overwrite      = args.overwrite
+    assets_path    = os.path.join(jekyll_folder, "assets")
+    posts_path     = os.path.join(jekyll_folder, "_posts")
 
+    # Check if filepaths exist
     if not os.path.exists(ipynb_filepath):
         parser.error('File not found: {}'.format(ipynb_filepath))
     if not os.path.exists(assets_path):
         parser.error('Folder not found: {}'.format(assets_path))
-    if not os.path.exists(assets_path):
-        parser.error('Folder not found: {}'.format(assets_path))
+    if not os.path.exists(posts_path):
+        parser.error('Folder not found: {}'.format(posts_path))
 
-    print('Ipynb filepath : {}'.format(ipynb_filepath))
-    print('Assets path    : {}'.format(assets_path))
-    print('Posts path     : {}'.format(posts_path))
-
-    # Get filename and folder from jupyter notebook path
-    ipynb_filename = os.path.splitext(os.path.basename(ipynb_filepath))[0]
-    ipynb_folder = os.path.dirname(ipynb_filepath)
-    image_folder_name = ipynb_filename.replace('-', '_') + "_files"
-
-    # Load notebook and convert to markdown
-    markdown_exporter = nbconvert.MarkdownExporter()
-    with open(ipynb_filepath, 'r') as f:
-        nb = nbformat.reads(f.read(), as_version=4)
-
-    front_matter = ""
-    first_cell = nb.cells[0]
-    # Chech if metadata has been added to first cell
-    if first_cell.metadata:
-        # Get title from first cell
-        title = first_cell.source.replace('#', '').strip()
-
-        # Generate front matter from metadata
-        front_matter = "---\n"
-        front_matter += "title: {}\n".format(title)
-        for key, value in first_cell.metadata.items():
-            front_matter += "{}: {}\n".format(key, value)
-
-        front_matter += "---\n"
-
-        print('Front matter created from metadata : ')
-        print(front_matter)
-
-        # Remove first cell
-        nb.cells.remove(first_cell)
-
-    # Remove all cells marked hidden
-    for cell in list(nb.cells):
-        if cell.metadata and cell.metadata.get('hide', False):
-            nb.cells.remove(cell)
-
-    (body, resources) = markdown_exporter.from_notebook_node(nb)
-    body = front_matter + body
-
-    # Get current date
-    if current_time:
-        dt = datetime.datetime.now()
-        files = [f for f in os.listdir(posts_path) if ipynb_filename in f]
-        if len(files) == 1:
-             os.remove(os.path.join(posts_path, files[0]))
-        elif len(files) > 1:
-            raise Exception('Multiple files named the same way with different dates')
+    # Check if filepath is directory
+    if os.path.isdir(ipynb_filepath):
+        print("filepath is directory")
+        for filename in os.listdir(ipynb_filepath):
+            if filename.endswith('.ipynb'):
+                filepath = os.path.join(ipynb_filepath, filename)
+                print('Converting filepath:', filepath)
+                try:
+                    parse.parse(filepath, assets_path, posts_path, overwrite)
+                except Exception as e:
+                    print("ERROR: {}, for {}".format(e, filepath))
     else:
-        files = [f for f in os.listdir(posts_path) if ipynb_filename in f]
-        if len(files) == 0:
-            dt = datetime.datetime.now()
-        elif len(files) == 1:
-            dt = datetime.datetime.strptime(files[0][:10], '%Y-%m-%d')
-        else:
-            raise Exception('Multiple files named the same way with different dates')
+        print('Converting filepath:', ipynb_filepath)
+        parse.parse(ipynb_filepath, assets_path, posts_path, overwrite)
 
-    markdown_filepath = os.path.join(posts_path, '{}-{}.md'.format(
-            dt.strftime('%Y-%m-%d'), ipynb_filename))
-
-    # Remove first and last line breaks
-    body = body.strip()
-
-    # Find all image links
-    pattern = r'\!\[.*?\]\((.+?)\)'
-    images = re.findall(pattern, body)
-
-    # Replace image links
-    def image_replace(match):
-        image_title, image_path = match.groups()
-        return '![{}]({{{{ site.baseurl }}}}/{}/{}/{})'.format(
-            image_title, assets_folder,
-            image_folder_name, image_path.split('/')[-1])
-
-    pattern = r'\!\[(.*?)\]\((.+?)\)'
-    #new_pattern = r'![\1]({{{{ site.baseurl }}}}/{}/{}/\2)'.format(assets_folder, image_folder_name)
-    body = re.sub(pattern, image_replace, body)
-
-    # Replace single dollar signs for inline equations
-    pattern = r'(?<!\$)\$(?!\$)'
-    body = re.sub(pattern, '$$', body)
-
-    # Wirte markdown post
-    print('Writing post  : {}'.format(markdown_filepath))
-    with open(markdown_filepath, 'w') as f:
-        f.write(body)
-
-    # Check if there are any images
-    if images:
-        # Remove existing image folder and create new one
-        images_folder = os.path.join(assets_path, image_folder_name)
-        if os.path.exists(images_folder):
-            shutil.rmtree(images_folder)
-        os.mkdir(images_folder)
-
-        # Write images
-        for image in images:
-            image_path = os.path.join(images_folder, image.split('/')[-1])
-            print('Writing image : {}'.format(image))
-
-            print(image_path)
-
-            if image in resources['outputs']:
-                data = resources['outputs'][image]
-                with open(image_path, 'wb') as f:
-                    f.write(data)
-            else:
-                shutil.copy2(os.path.join(ipynb_folder, image), image_path)
+if __name__ == '__main__':
+    main()
---
+++ jmconvert/parse.py
@@ -0,0 +1,118 @@
+import os
+import re
+import shutil
+import datetime
+import nbformat
+import nbconvert
+
+
+EXCLUDE_METADATA = ['language_info', 'kernelspec', 'date']
+
+
+def parse(ipynb_filepath, assets_path, posts_path, overwrite=False):
+    # Get filename and folder from jupyter notebook path
+    ipynb_filename = os.path.splitext(os.path.basename(ipynb_filepath))[0]
+    ipynb_folder = os.path.dirname(ipynb_filepath)
+    image_folder_name = ipynb_filename.replace('-', '_') + "_files"
+    images = []
+
+    # Load notebook and convert to markdown
+    markdown_exporter = nbconvert.MarkdownExporter()
+    with open(ipynb_filepath, 'r') as f:
+        nb = nbformat.reads(f.read(), as_version=4)
+
+
+    # Get title from first cell and remove cell
+    first_cell = nb.cells.pop(0)
+    title = first_cell.source.replace('#', '').strip()
+
+    # Generate front matter from metadata
+    front_matter = "---\n"
+    front_matter += "title: \"{}\"\n".format(title)
+    for key, value in nb.metadata.items():
+        if key not in EXCLUDE_METADATA:
+            if key == 'image':
+                images.append(value)
+                value = '/assets/{}/{}'.format(
+                    image_folder_name, value.split('/')[-1])
+
+            front_matter += "{}: {}\n".format(key, value)
+    front_matter += "---\n"
+
+    print('Front matter created from metadata : ')
+    print(front_matter)
+
+
+    # Remove all cells marked hidden
+    for cell in list(nb.cells):
+        if cell.metadata and cell.metadata.get('hide', False):
+            nb.cells.remove(cell)
+
+    (body, resources) = markdown_exporter.from_notebook_node(nb)
+    body = front_matter + body
+
+
+    # Get current date
+    if 'date' in nb.metadata:
+        dt = datetime.datetime.strptime(nb.metadata['date'], '%Y-%m-%d')
+    else:
+        files = [f for f in os.listdir(posts_path) if ipynb_filename in f]
+        if len(files) == 0:
+            dt = datetime.datetime.now()
+        elif len(files) == 1:
+            dt = datetime.datetime.strptime(files[0][:10], '%Y-%m-%d')
+        else:
+            raise Exception(
+                'Multiple files named the same way with different dates')
+
+    markdown_filepath = os.path.join(posts_path,
+        '{}-{}.md'.format(dt.strftime('%Y-%m-%d'), ipynb_filename))
+
+    # Remove first and last line breaks
+    body = body.strip()
+
+
+    # Find all image links
+    pattern = r'\!\[.*?\]\((.+?)\)'
+    images.extend(re.findall(pattern, body))
+    print('Images:', images)
+
+    # Replace image links
+    def image_replace_link(match):
+        image_title, image_path = match.groups()
+        return '![{}]({{{{ site.baseurl }}}}/{}/{}/{})'.format(
+            image_title, "assets",
+            image_folder_name, image_path.split('/')[-1])
+
+    pattern = r'\!\[(.*?)\]\((.+?)\)'
+    body = re.sub(pattern, image_replace_link, body)
+
+    # Replace single dollar signs for inline equations
+    pattern = r'(?<!\$)\$(?!\$)'
+    body = re.sub(pattern, '$$', body)
+
+    # Wirte markdown post
+    print('Writing post  : {}'.format(markdown_filepath))
+    with open(markdown_filepath, 'w') as f:
+        f.write(body)
+
+    # Check if there are any images
+    if images:
+        # Remove existing image folder and create new one
+        images_folder = os.path.join(assets_path, image_folder_name)
+        if os.path.exists(images_folder):
+            shutil.rmtree(images_folder)
+        os.mkdir(images_folder)
+
+        # Write images
+        for image in images:
+            image_path = os.path.join(images_folder, image.split('/')[-1])
+            print('Writing image : {}'.format(image))
+            print('Image path:', image_path)
+
+            if image in resources['outputs']:
+                data = resources['outputs'][image]
+                with open(image_path, 'wb') as f:
+                    f.write(data)
+            else:
+                shutil.copy2(os.path.join(ipynb_folder, image), image_path)
